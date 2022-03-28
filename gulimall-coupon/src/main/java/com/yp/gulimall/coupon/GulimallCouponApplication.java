773e2fc8ecd7e6cb12d8ccee31e9afd54732cd50
painye <1850384745@qq.com>
2022-03-28 21:12:31 +0800

    coupon测试成功

--- gulimall-coupon/src/main/java/com/yp/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/yp/gulimall/coupon/GulimallCouponApplication.java
@@ -1,13 +1,22 @@
 package com.yp.gulimall.coupon;
+/**
+ * @author pan
+ * @date 2022/3/28 21:02
+ */
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @ClassName : com.yp.gulimall.coupon.GulimallCouponApplication
+ * @Description : 类描述
+ * @author pan
+ * @date 2022/3/28 21:02
+ */
+
 @SpringBootApplication
 public class GulimallCouponApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GulimallCouponApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class,args);
+    }
 }
