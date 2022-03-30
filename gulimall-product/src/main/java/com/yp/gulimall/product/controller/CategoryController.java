1664478aaa084ed47aa2f56a49eb895623500f1a
painye <1850384745@qq.com>
2022-03-30 13:18:18 +0800

    gateway路由验证码成功

---
+++ gulimall-product/src/main/java/com/yp/gulimall/product/controller/CategoryController.java
@@ -0,0 +1,89 @@
+package com.yp.gulimall.product.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+//import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yp.gulimall.product.entity.CategoryEntity;
+import com.yp.gulimall.product.service.CategoryService;
+import com.yp.common.utils.PageUtils;
+import com.yp.common.utils.R;
+
+
+
+/**
+ * 商品三级分类
+ *
+ * @author painye
+ * @email sunlightcs@gmail.com
+ * @date 2022-03-29 21:07:19
+ */
+@RestController
+@RequestMapping("product/category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 查出商场中的所有分类以及子类，艺术性结构组装起来
+     */
+    @RequestMapping("/list/tree")
+    public R list(@RequestParam Map<String, Object> params){
+        List<CategoryEntity> entities= categoryService.listWithTree();
+        return R.ok().put("data", entities);
+    }
+
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{catId}")
+    //@RequiresPermissions("product:category:info")
+    public R info(@PathVariable("catId") Long catId){
+		CategoryEntity category = categoryService.getById(catId);
+
+        return R.ok().put("category", category);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    //@RequiresPermissions("product:category:save")
+    public R save(@RequestBody CategoryEntity category){
+		categoryService.save(category);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    //@RequiresPermissions("product:category:update")
+    public R update(@RequestBody CategoryEntity category){
+		categoryService.updateById(category);
+
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    //@RequiresPermissions("product:category:delete")
+    public R delete(@RequestBody Long[] catIds){
+		categoryService.removeByIds(Arrays.asList(catIds));
+
+        return R.ok();
+    }
+
+}
--- gulimall-product/src/main/java/com/yp/gulimall/product/service/CategoryService.java
+++ gulimall-product/src/main/java/com/yp/gulimall/product/service/CategoryService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.yp.common.utils.PageUtils;
 import com.yp.gulimall.product.entity.CategoryEntity;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,5 +17,12 @@ import java.util.Map;
 public interface CategoryService extends IService<CategoryEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+
+    /**
+     * 返回按级别划分的树形结构
+     * @return
+     */
+    List<CategoryEntity> listWithTree();
 }
 
