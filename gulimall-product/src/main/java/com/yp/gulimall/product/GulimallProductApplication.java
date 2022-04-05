90e25752adeb46f3d40cc928e0bd8a45401e99b5
painye <1850384745@qq.com>
2022-04-05 23:01:06 +0800

    仓库模块配置路由

--- gulimall-product/src/main/java/com/yp/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/yp/gulimall/product/GulimallProductApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 1、整合MyBatis-plus
@@ -33,6 +34,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 
 
+@EnableTransactionManagement
 @EnableFeignClients(basePackages = "com.yp.gulimall.product.feign")
 @SpringBootApplication
 @MapperScan("com.yp.gulimall.product.dao")
--- gulimall-ware/src/main/java/com/yp/gulimall/ware/service/impl/WareInfoServiceImpl.java
+++ gulimall-ware/src/main/java/com/yp/gulimall/ware/service/impl/WareInfoServiceImpl.java
@@ -11,6 +11,7 @@ import com.yp.common.utils.Query;
 import com.yp.gulimall.ware.dao.WareInfoDao;
 import com.yp.gulimall.ware.entity.WareInfoEntity;
 import com.yp.gulimall.ware.service.WareInfoService;
+import org.springframework.util.StringUtils;
 
 
 @Service("wareInfoService")
@@ -18,9 +19,17 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareInfoEntity> wareInfoEntityQueryWrapper = new QueryWrapper<>();
+        String key = (String)params.get("key");
+        if(!StringUtils.isEmpty(key)){
+            wareInfoEntityQueryWrapper.eq("id", key)
+                    .or().like("address", key)
+                    .or().like("areacode", key);
+        }
+
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                wareInfoEntityQueryWrapper
         );
 
         return new PageUtils(page);
